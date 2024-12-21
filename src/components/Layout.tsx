97a95881533cfe1b979e22690ec84a5632b5b574
VARA PRASAD SRIRAM <bhargav200794@gmail.com>
2024-12-22 00:08:18 +0530

    change UI

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Sidebar } from './Sidebar';
-import { TopNav } from './TopNav';
+
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,13 +8,7 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <TopNav />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 p-6">
-          <Outlet />
-        </main>
-      </div>
+        
       {children}
     </div>
   );
